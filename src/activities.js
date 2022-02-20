dd53d2663557d1c9b23a1eca63483a46346e6ab4
david_chromeOS <david@buho.academy>
2022-02-19 20:44:50 -0600

    📝 edit tasks

--- src/activities.js
+++ src/activities.js
@@ -7,27 +7,27 @@ const activities = [
 	},
 	{
 		id: 2,
-		date: "01/18/2022",
+		date: "02/20/2022",
 		subject: "Inglés Técnico I",
-		activity: "Laboratorio No. 1",
+		activity: "Desarrollo diapositivas 7,8,9 y libro 2 - 3(D,E,F)",
 	},
 	{
 		id: 3,
-		date: "02/18/2022",
+		date: "02/23/2022",
 		subject: "Paradigmas de Programación",
-		activity: "Laboratorio No. 1",
+		activity: "Generalidades de la programación",
 	},
 	{
 		id: 4,
-		date: "02/18/2022",
+		date: "02/24/2022",
 		subject: "Matemática Aplicada I",
-		activity: "Laboratorio No. 1",
+		activity: "Guía parte I y II",
 	},
 	{
 		id: 5,
-		date: "02/18/2022",
+		date: "",
 		subject: "Introducción a las bases de datos",
-		activity: "Laboratorio No. 1",
+		activity: "",
 	},
 ];
 
